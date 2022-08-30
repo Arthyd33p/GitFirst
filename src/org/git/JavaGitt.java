9e9a969d1c865c51f3126f83d2dfcbe3ac711a0e
Arthyd33p <arthyd33p@gmail.com>
2022-08-30 23:49:48 +0300

    code were added by Adi

--- src/org/git/JavaGitt.java
+++ src/org/git/JavaGitt.java
@@ -1,6 +1,10 @@
 package org.git;
 
 public class JavaGitt {
+	public void m2() {
+	System.out.println("Adi Code added.....");
+
+	}
 	public static void main(String[] args) {
 		System.out.println("Arthy.......");
 		System.out.println("Successfull...............");
