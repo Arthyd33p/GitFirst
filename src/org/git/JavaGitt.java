36dba1255ba4125c72f3c08a2ef318091d9ba3a3
Arthyd33p <arthyd33p@gmail.com>
2022-08-30 23:41:32 +0300

    code were added by Art

--- src/org/git/JavaGitt.java
+++ src/org/git/JavaGitt.java
@@ -1,6 +1,9 @@
 package org.git;
 
 public class JavaGitt {
+	public void m1(){
+		System.out.println("Siva added code....");
+	}
 	public static void main(String[] args) {
 		System.out.println("Arthy.......");
 		System.out.println("Successfull...............");
