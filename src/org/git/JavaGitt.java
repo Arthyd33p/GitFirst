6d124acc314784930a250beb8f5aa9736f51cea8
Arthyd33p <arthyd33p@gmail.com>
2022-08-30 12:36:26 +0300

    Lines of code  added

---
+++ src/org/git/JavaGitt.java
@@ -0,0 +1,9 @@
+package org.git;
+
+public class JavaGitt {
+	public static void main(String[] args) {
+		System.out.println("Arthy.......");
+		System.out.println("Successfull...............");
+	}
+
+}
